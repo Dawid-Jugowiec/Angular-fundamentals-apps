8213136284b9c765ad93f2ba978060e54b055f42
Dawid <d.j.w.jugowiec@gmail.com>
2019-04-17 13:46:55 +0200

    Fixed vulnerability

--- component-lifecycle-and-communication-angular/e2e/src/app.e2e-spec.ts
+++ component-lifecycle-and-communication-angular/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to deep-databinding!');
+    expect(page.getTitleText()).toEqual('Welcome to life-cycle-app!');
   });
 
   afterEach(async () => {
